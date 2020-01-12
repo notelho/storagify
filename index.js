8448a8f398737f86b0b995cdc97d32f3d4d6ba23
enbot <nimbotelho@gmail.com>
2020-01-12 00:50:18 -0300

    basic documentation

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 export * from './lib/controller/storagify.js'
+
+
+localStorage.setItem('teste', { a: 'b' })
+localStorage.setItem('teste2', 'teste2')
+localStorage.setItem('teste3', null)
+
+console.log(localStorage.getItem('teste'))
+console.log(localStorage.getItem('teste2'))
+console.log(localStorage.getItem('teste3'))
+
+setTimeout(() => {
+
+    console.warn('default to prod:');
+    storagify.init('test')
+
+    console.log(localStorage.getItem('teste'))
+    console.log(localStorage.getItem('teste2'))
+    console.log(localStorage.getItem('teste3'))
+
+    setTimeout(() => {
+
+        console.warn('prod to dev:');
+        storagify.init('test', { dev: true })
+
+        console.log(localStorage.getItem('teste'))
+        console.log(localStorage.getItem('teste2'))
+        console.log(localStorage.getItem('teste3'))
+
+        setTimeout(() => {
+
+            console.warn('dev to prod:');
+            storagify.init('test')
+
+            console.log(localStorage.getItem('teste'))
+            console.log(localStorage.getItem('teste2'))
+            console.log(localStorage.getItem('teste3'))
+
+        }, 2000)
+    }, 2000)
+}, 2000)

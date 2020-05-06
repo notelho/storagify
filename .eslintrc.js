0d0a84f0769c40a67b14cb0d521d61602e350c70
enbot <nimbotelho@gmail.com>
2020-05-06 20:39:13 -0300

    update gitignore, editorconfig and webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  'parser': '@typescript-eslint/parser',
-  'extends': [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  'parserOptions': {
-    'sourceType': 'module',
-  },
-  'plugins': [
-    '@typescript-eslint',
-  ]
+	'parser': '@typescript-eslint/parser',
+	'extends': [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	'parserOptions': {
+		'sourceType': 'module',
+	},
+	'plugins': [
+		'@typescript-eslint',
+	]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "umd"),
-    filename: "storagify.js",
-    library: "storagify",
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: "tsconfig.umd.json"
-          }
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+	mode: "production",
+	entry: "./src/index.d.ts",
+	output: {
+		path: path.resolve(__dirname, "umd"),
+		filename: "storagify.js",
+		library: "storagify",
+		libraryTarget: "umd"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "ts-loader",
+					options: {
+						configFile: "tsconfig.umd.json"
+					}
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"]
+	}
 };
